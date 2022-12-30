1efc1003c72ab8c01f2e0cd9ba46cb5156f624be
Yehor <egorkorsun7@gmail.com>
2022-12-30 17:44:16 +0100

    Добавьте файлы проекта.

---
+++ Exam_Test/Exam_Test.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+
+#include"Admin.h"
+#include"Tester.h"
+#include"Test.h"
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
---
+++ Exam_Test/Tester.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "People.h"
+
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+class Tester : public People
+    
+{
+};
+
